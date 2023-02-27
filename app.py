50a72b6aeca49a1f55d4ca4a0798bbf240b4d200
Jyothigeo <126162684+Jyothigeo@users.noreply.github.com>
2023-02-27 10:40:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Hi, I am Jyothi Prativadi :wave:")
+st.header("Data scientist")
+st.write( "A Data enthusiast with strong passion towards delivering object oriented and high standard data driven results that contribute to the organizations value addition"
+    )
